f55b51e47cb84c7fc25ab31a53e6055e2972d281
Alberto Sainz Vegas <nuborisar@gmail.com>
2017-03-09 09:23:44 +0100

    change reflection

--- src/com/wagnerandade/coollection/reflection/Phanton.java
+++ src/com/wagnerandade/coollection/reflection/Phanton.java
@@ -3,35 +3,35 @@ package com.wagnerandade.coollection.reflection;
 import java.lang.reflect.Field;
 
 public class Phanton<T> {
-	
-	private final T target;
-	private Class<?> clazz;
 
-	private Phanton(T target) {
-		this.target = target;
-		clazz = target.getClass();
-	}
-	
-	public static <T> Phanton<T> from(T target) {
-		return new Phanton<T>(target);
-	}
-	
-	public Object call(String name) {
-		return invoke(name);
-	}
+  private final T target;
+  private Class<?> clazz;
 
-	private Object invoke(String name){
-		for (final Field field : clazz.getDeclaredFields()) {
-			try {
-				if (name.equals(field.getName())){
-					field.setAccessible(Boolean.TRUE);
-					return field.get(target);
-				}
-			} catch (final IllegalAccessException e) {
-				throw new RuntimeException(e);
-			}
-		}
-		throw new RuntimeException("No such property with name " + name);
-	}
-	
+  private Phanton(T target) {
+    this.target = target;
+    clazz = target.getClass();
+  }
+
+  public static <T> Phanton<T> from(T target) {
+    return new Phanton<T>(target);
+  }
+
+  public Object call(String name) {
+    return invoke(name);
+  }
+
+  private Object invoke(String name) {
+    for (final Field field : clazz.getDeclaredFields()) {
+      try {
+                if (name.toLower().equals(field.getName().toLower()) || "get" + name.equals(field.getName())
+             ) {
+          field.setAccessible(Boolean.TRUE);
+          return field.get(target);
+        }
+      } catch (final IllegalAccessException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    throw new RuntimeException("No such property with name " + name);
+  }
 }
