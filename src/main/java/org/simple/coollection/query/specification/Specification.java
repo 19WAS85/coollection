2f289bce25e6ce4ca56df29ddce2bf9712295982
Vinicius Costa <viniciusrdacosta@gmail.com>
2018-03-06 00:22:45 -0300

    Remove unnecessary keywords

--- src/main/java/org/simple/coollection/query/specification/Specification.java
+++ src/main/java/org/simple/coollection/query/specification/Specification.java
@@ -4,6 +4,6 @@ import org.simple.coollection.query.criteria.Criteria;
 
 public interface Specification<T> {
 
-  public boolean match(T item, Criteria<T> one, Criteria<T> other);
+  boolean match(T item, Criteria<T> one, Criteria<T> other);
 
 }
